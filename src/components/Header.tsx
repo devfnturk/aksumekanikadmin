041cdec82e1712df38f6a188e8279df6d859efbe
devfnturk <devfnturk@gmail.com>
2025-10-03 00:24:37 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -79,7 +79,6 @@ const Header: React.FC = () => {
                                     <Link to="/dataOfAksuYonetimi" className="block px-4 py-2 hover:bg-gray-100">Data Of Aksu Yönetimi</Link>
                                     <Link to="/catalogYönetimi" className="block px-4 py-2 hover:bg-gray-100">Katalog Yönetimi</Link>
                                     <Link to="/referansYonetimi" className="block px-4 py-2 hover:bg-gray-100">Referans Yönetimi</Link>
-                                    <Link to="/fieldOfActivitiesYonetimi" className="block px-4 py-2 hover:bg-gray-100">Field Of Activities Yönetimi</Link>
                                     <Link to="/brandYonetimi" className="block px-4 py-2 hover:bg-gray-100">Marka Yönetimi</Link>
                                     <Link to="/brandActivityAreasYonetimi" className="block px-4 py-2 hover:bg-gray-100">Marka Etkinlikleri Alanı Yönetimi</Link>
                                 </div>
